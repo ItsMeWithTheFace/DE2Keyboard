d5586d8d6a1a01e646ff94f37a8c51e6642d53f5
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-03-14 16:40:30 -0400

    Fixed the issue with the GPIO ports

--- Top.v
+++ Top.v
@@ -1,12 +1,12 @@
-module Top(CLOCK_50, KEY, GPIO);
+module Top(CLOCK_50, SW, GPIO);
     input CLOCK_50;
-    input [3:0] KEY;
-    output [4:0] GPIO;  // Speaker is connected to this, need to change
+    input [2:0] SW;
+    output [35:0] GPIO;  // Speaker is connected to this
 
     // Instantiate the note player module
     SingleNotePlayer player(
         .clk(CLOCK_50),
-        .note(KEY[3:0]),
-        .speaker(GPIO[4])
+        .note(SW[2:0]),
+        .speaker(GPIO[7:0])
     );
 endmodule
