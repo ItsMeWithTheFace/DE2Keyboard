8486be3536908f9f17ebe10836b8537c5b786275
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-03-14 19:58:55 -0400

    Added an LEDR parameter to see the counter

--- Top.v
+++ Top.v
@@ -1,12 +1,15 @@
-module Top(CLOCK_50, SW, GPIO);
+module Top(CLOCK_50, KEY, LEDR, GPIO);
     input CLOCK_50;
-    input [2:0] SW;
-    output [35:0] GPIO;  // Speaker is connected to this
+    input [2:0] KEY;
+	output [18:0] LEDR;
+    output [7:0] GPIO;  // Speaker is connected to this
 
     // Instantiate the note player module
     SingleNotePlayer player(
         .clk(CLOCK_50),
-        .note(SW[2:0]),
-        .speaker(GPIO[7:0])
+        .note(~KEY[2:0]),
+		.freq_out(LEDR[18:0]),
+        .speaker(GPIO)
     );
+
 endmodule
