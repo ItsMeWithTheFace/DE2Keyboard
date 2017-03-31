14becf5ce770c718a94783ee415427730e6755ff
Rakin Uddin <rakin.uddin@mail.utoronto.ca>
2017-03-31 16:15:29 -0400

    Added old version of Top level module

---
+++ Top_Old.v
@@ -0,0 +1,19 @@
+module Top_Old(CLOCK_50, SW, KEY, LEDG, LEDR, GPIO);
+    input CLOCK_50;
+    input [2:0] SW;
+    input [3:0] KEY;
+    output [2:0] LEDG;
+    output [18:0] LEDR;
+    output [7:0] GPIO;  // Speaker is connected to this
+
+    // Instantiate the note player module
+    SingleNotePlayerLayered player(
+        .clk(CLOCK_50),
+        .state(SW),
+        .note(~KEY),
+        .layer_out(LEDG),
+        .freq_out(LEDR),
+        .speaker(GPIO)
+    );
+
+endmodule
